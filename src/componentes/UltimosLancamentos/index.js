0b99be0d751da1595f7cb763405762ece09f67ac
SannaDias <sannapdias@gmail.com>
2025-02-03 19:20:50 -0300

    props

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,21 +1,14 @@
 import { livros } from './dadosUltimosLancamentos'
 import styled from 'styled-components'
+import CardRecomenda from '../CardRecomendo'
+import { Titulo } from '../Titulo'
+import imagemLivro from '../../imagens/livro2.png'
 
 const UltimosLancamentosContainer = styled.section`
     background-color: #EBECEE;
     padding-bottom: 20px;
     display: flex;
     flex-direction: column;
-`
-
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
 `
 
 const NovosLivrosContainer = styled.div`
@@ -29,12 +22,20 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos(){
     return(
         <UltimosLancamentosContainer>
-            <Titulo>ULTIMOS LANÇAMENTOS</Titulo>
+            <Titulo cor="#EB9B00"
+            tamanhoFonte='36px'
+            >ULTIMOS LANÇAMENTOS</Titulo>
             <NovosLivrosContainer>
                 {livros.map(livro => (
                     <img src={livro.src}></img>
                 ))}
             </NovosLivrosContainer>
+            <CardRecomenda
+               titulo="Talvez você se interesse por"
+               subtitulo="Angular 11"
+               descricao="Construindo uma aplicação com a plataforma Google"
+               img={imagemLivro}
+            />
         </UltimosLancamentosContainer>
     )
 }
